1803567fe3b12b3cd921ca7198d09e0f1d8201a2
JoeZlonicky <joe.zlonicky@gmail.com>
2024-08-23 13:59:19 -0700

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import { handle404 } from './middleware/handle404.js';
 import { handle500 } from './middleware/handle500.js';
 import { IndexRouter } from './routes/Index.router.js';
+import cors from 'cors';
 import express from 'express';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
